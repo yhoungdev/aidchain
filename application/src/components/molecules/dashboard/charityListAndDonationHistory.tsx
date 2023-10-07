19362eecced48573c7722fbb9e1381b39cf366a7
yhoungdev <yhoungdev@gmail.com>
2023-10-07 08:21:13 +0100

    chore: (UI) included history UI

--- application/src/components/molecules/dashboard/charityListAndDonationHistory.tsx
+++ application/src/components/molecules/dashboard/charityListAndDonationHistory.tsx
@@ -1,21 +1,36 @@
 import Container from '@/app/_layout/container';
 import { BORDER_STYLE } from '@/constant/style_constant';
-import React from 'react';
+import React, { Fragment } from 'react';
+import CharityItems from './charityItem';
+import HistoryBox from './donatedBox';
 
 const CharityListAndDonationHistory = () => {
   return (
     <Container>
       <div className='flex gap-5] gap-5'>
-        <div className='px-5 py-[1em]' style={BORDER_STYLE}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          facere corporis ullam eaque sit perferendis fugit rem ipsum, animi ab
-          accusamus quam natus modi fugiat dolor dolorum velit sunt possimus.
+        <div className='px-5 py-[1em] lg:w-[600px] ' style={BORDER_STYLE}>
+          <h1 className='font-[500] my-4'>List of Charity Organizations</h1>
+
+          {/* list charities for useres */}
+          <div>
+            {[1, 2, 2].map((_) => (
+              <Fragment key={_}>
+                <CharityItems />
+              </Fragment>
+            ))}
+          </div>
         </div>
 
-        <div className='px-5 py-[1em]' style={BORDER_STYLE}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          facere corporis ullam eaque sit perferendis fugit rem ipsum, animi ab
-          accusamus quam natus modi fugiat dolor dolorum velit sunt possimus.
+        <div className='px-5 py-[1em] lg:w-[842px]' style={BORDER_STYLE}>
+          <h1 className='font-[500] my-4'>List of Charity Donated To</h1>
+
+          <div className='my-5'>
+            {[1, 2, 2].map((_) => (
+              <Fragment key={_}>
+                <HistoryBox />
+              </Fragment>
+            ))}
+          </div>
         </div>
       </div>
     </Container>
